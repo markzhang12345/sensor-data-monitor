0344fc58df1a3af00fb2c76a55f129848be1af8d
kpmark <2585050765@qq.com>
2024-11-13 22:44:29 +0800

    feat: 新增后端操作日志请求处理功能

--- server/routes/action.js
+++ server/routes/action.js
@@ -1,4 +1,5 @@
 let express = require("express");
+const Action = require("../models/Action");
 let { sendCommand } = require("../connect/mqtt");
 let router = express.Router();
 
@@ -8,6 +9,19 @@ router.post("/action", (req, res) => {
   sendCommand(command);
   if (command) res.status(200).send("command sent");
   else res.status(400).send("Cannot receive command");
+});
+
+router.get("/action/log", (req, res) => {
+  Action.find()
+    .sort({ time: -1 })
+    .limit(20)
+    .then((actions) => {
+      res.status(200).json(actions);
+    })
+    .catch((err) => {
+      console.error("Error fetching actions", err);
+      res.status(500).send("Internal Server Error");
+    });
 });
 
 module.exports = router;
